7a80fba537cbf04c3df898bb9389d9834fd49b5b
Luckie Musngi <luckie.musngi@gmail.com>
2025-05-04 22:27:42 -0700

    added dbconn

--- main/src/DBMSSetup.java
+++ main/src/DBMSSetup.java
@@ -1,5 +1,71 @@
+// Authors: Luckie Musngi and Aj Becerra
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBMSSetup {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Connection dbconn = getDbconn(); // connect to the database
     }
+
+    // * gets and returns a connection to the database
+    private static Connection getDbconn() {
+        final String oracleURL
+                = // Magic lectura -> aloe access spell
+                "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
+
+        String username = null, // Oracle DBMS username
+                password = null;    // Oracle DBMS password
+
+        try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
+            System.out.print("Enter your Oracle DBMS username: ");
+            username = scanner.nextLine();
+            System.out.print("Enter your Oracle DBMS password: ");
+            password = scanner.nextLine();
+        } catch (Exception e) {
+            System.err.println("*** Exception:  " + e.getMessage());
+        }
+
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+
+        } catch (ClassNotFoundException e) {
+
+            System.err.println("*** ClassNotFoundException:  "
+                    + "Error loading Oracle JDBC driver.  \n"
+                    + "\tPerhaps the driver is not on the Classpath?");
+            System.exit(-1);
+        }
+
+        // make and return a database connection to the user's
+        // Oracle database
+        Connection dbconn = null;
+
+        try {
+            dbconn = DriverManager.getConnection(oracleURL, username, password);
+        } catch (SQLException e) {
+
+            System.err.println("*** SQLException:  "
+                    + "Could not open JDBC connection.");
+            System.err.println("\tMessage:   " + e.getMessage());
+            System.err.println("\tSQLState:  " + e.getSQLState());
+            System.err.println("\tErrorCode: " + e.getErrorCode());
+            System.exit(-1);
+        }
+
+        return dbconn;
+    }
+
+    // * Table Creation ---------------------------------------------------------------------------
+    // Tables to be Made:
+    // member, ski pass, gear rental, equipment, equipment update       
+    private static void makeTables(Connection dconn) {
+        
+    }
+
+
+
+    // * Table Creation (End) ---------------------------------------------------------------------
 }
